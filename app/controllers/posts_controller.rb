4db56849e25eaf296d95333fab034c5242d1ab30
SudoRudo <r.guerreronyc@gmail.com>
2020-06-24 11:50:50 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,28 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+
+  end
   
+  def create
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+
+    redirect_to posts_path
+  end
 end
+
+
+# Form for raw html 
+# <form>
+#   <label>Post title:</label><br>
+#   <input type="text" id="post_title" name="post[title]"><br>
+ 
+#   <label>Post description:</label><br>
+#   <textarea id="post_description" name="post[description]"></textarea><br>
+ 
+#   <input type="submit" value="Submit Post">
+# </form>
+ 
+# <%= params.inspect %>
